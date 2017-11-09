c5ad44a57038bf57354f6578debacd4e6296c65b
lac <laclys@126.com>
2017-11-09 23:39:55 +0800

    UILabel控件

--- swiftStudy/swiftStudy/ViewController.swift
+++ swiftStudy/swiftStudy/ViewController.swift
@@ -17,6 +17,23 @@ class ViewController: UIViewController {
         label.text = "test"
         // 将其放到视图内
         self.view.addSubview(label)
+        // 创建了一个自定义UILabel控件
+        let label2 = UILabel(frame: CGRect(x:20, y: 60,width: 200, height: 30))
+        label2.text = "这里是文本2"
+        label2.font = UIFont.boldSystemFont(ofSize: 20)
+        label2.textColor = UIColor.red
+        // 阴影颜色
+        label2.shadowColor = UIColor.green
+        // 阴影位移
+        label2.shadowOffset = CGSize(width: 2, height: 2)
+        // 文字对齐方式
+        label2.textAlignment = NSTextAlignment.center
+        self.view.addSubview(label2)
+        let label3 = UILabel(frame: CGRect(x:20, y: 100, width: 200, height: 130))
+        label3.text = "长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本长文本"
+        // 设置显示行数
+        label3.numberOfLines = 3
+        self.view.addSubview(label3)
     }
 
     override func didReceiveMemoryWarning() {
