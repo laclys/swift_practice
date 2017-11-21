b4df528bfaa989c54dc2717bbc1518637c2ee4e3
lac <laclys@126.com>
2017-11-21 22:53:48 +0800

    抽屉风格弹窗控制器

--- UIAlertTest/UIAlertTest/ViewController.swift
+++ UIAlertTest/UIAlertTest/ViewController.swift
@@ -29,4 +29,17 @@ class ViewController: UIViewController {
         self.present(alertController,animated: true,completion: nil)
     }
 
+    @IBAction func pop2(_ sender: AnyObject) {
+        // 使用抽屉弹窗风格的警告控制器
+        let alertController = UIAlertController(title: "抽屉弹窗", message: "写点什么内容好捏~", preferredStyle: .actionSheet)
+        let alertAction1 = UIAlertAction(title: "Yes", style: .default)
+        { (action) in
+                        print(alertController.textFields?.first?.text ?? "写点什么嘛~")
+        }
+        let alertAction2 = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(alertAction1)
+        alertController.addAction(alertAction2)
+        self.present(alertController,animated: true,completion: nil)
+        
+    }
 }
