0606f58743fd85c3e73a015ceb2b118f40d27b2a
lac <laclys@126.com>
2017-11-21 21:49:19 +0800

    页面导航Navigation

---
+++ UINavigationControllerTest/UINavigationControllerTest/TableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TableViewController.swift
+//  UINavigationControllerTest
+//
+//  Created by lac on 2017/11/21.
+//  Copyright © 2017年 lac. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 设置导航栏颜色
+        self.navigationController?.navigationBar.barTintColor = UIColor.white
+        // 创建导航栏实例
+        let barItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(click))
+        self.navigationItem.leftBarButtonItem = barItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    func click () {
+        
+    }
+
+}
