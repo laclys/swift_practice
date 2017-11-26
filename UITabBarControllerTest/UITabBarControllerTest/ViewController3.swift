9522f274417575e07cc88c439b38957181cbfed9
lac <laclys@126.com>
2017-11-26 20:10:40 +0800

    TabBarItem

---
+++ UITabBarControllerTest/UITabBarControllerTest/ViewController3.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController3.swift
+//  UITabBarControllerTest
+//
+//  Created by lac on 2017/11/26.
+//  Copyright © 2017年 lac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 设置标题
+        self.tabBarItem.title = "首页"
+        self.view.backgroundColor = UIColor.red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UITabBarControllerTest/UITabBarControllerTest/ViewController4.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController4.swift
+//  UITabBarControllerTest
+//
+//  Created by lac on 2017/11/26.
+//  Copyright © 2017年 lac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController4: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 设置标题
+        self.tabBarItem.title = "UFC"
+        self.view.backgroundColor = UIColor.green
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
