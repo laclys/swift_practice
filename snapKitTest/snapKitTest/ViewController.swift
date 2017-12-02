0aa613ac6dd2408e37abe78ab2c1c080d521f26e
lac <laclys@126.com>
2017-12-03 01:22:05 +0800

    才用链式写法

--- snapKitTest/snapKitTest/ViewController.swift
+++ snapKitTest/snapKitTest/ViewController.swift
@@ -17,9 +17,8 @@ class ViewController: UIViewController {
         testView.backgroundColor = UIColor.cyan
         view.addSubview(testView)
         testView.snp.makeConstraints { (make) in
-            make.width.equalTo(100)         // 宽为100
-            make.height.equalTo(100)        // 高为100
-            make.center.equalTo(view)       // 位于当前视图的中心
+            make.width.height.equalTo(100)    // 链式语法直接定义宽高
+            make.center.equalToSuperview()    // 直接在父视图居中
         }
         
     }
